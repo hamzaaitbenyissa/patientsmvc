704acc4d9edb05efea1147b47a45f8286fb19b4e
Hamza AIT BEN YISSA <hamzaaitbenyissa@gmail.com>
2022-04-09 18:03:51 +0000

    use userDetailsService for security

--- src/main/java/ma/enset/patientsmvc/security/SecurityConfig.java
+++ src/main/java/ma/enset/patientsmvc/security/SecurityConfig.java
@@ -1,5 +1,7 @@
-package ma.enset.patientsmvc.sec;
+package ma.enset.patientsmvc.security;
 
+import ma.enset.patientsmvc.security.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,18 +11,41 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+/*    @Autowired
+    private DataSource dataSource;*/
+
+    @Autowired
+    PasswordEncoder passwordEncode;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        PasswordEncoder passwordEncoder = passwordEncoder();
+/*
+
         auth.inMemoryAuthentication().withUser("user1").password(passwordEncoder.encode("1234")).roles("USER");
         auth.inMemoryAuthentication().withUser("user2").password(passwordEncoder.encode("abcd")).roles("USER");
         auth.inMemoryAuthentication().withUser("admin").password(passwordEncoder.encode("admin")).roles("USER","ADMIN");
+*/
+/*
+        //JDBC authentication
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from users where username =?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);*/
+
+        auth.userDetailsService(userDetailsService);
+
     }
 
 
@@ -29,17 +54,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.formLogin();
         http.authorizeRequests().antMatchers("/").permitAll();
-
-        http.authorizeRequests().antMatchers("/delete/**", "/save/**", "/edit/**", "/formPatients/**", "/editPatients/**").hasRole("ADMIN");
-        http.authorizeRequests().antMatchers("/index/**").hasRole("USER");
+        http.authorizeRequests().antMatchers("/delete/**", "/save/**", "/edit/**", "/formPatients/**", "/editPatients/**").hasAuthority("ADMIN");
+        http.authorizeRequests().antMatchers("/index/**").hasAuthority("USER");
         http.authorizeRequests().anyRequest().authenticated();
         http.exceptionHandling().accessDeniedPage("/403");
+
     }
 
-    //create a password encoder with bycrypt algorithme and store it in context
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
 }
